651e966648d161b9ac7080292ecd88013cf5171e
Michael Washburn <mwashburn@hubspot.com>
2023-09-28 08:44:26 -0400

    spruce up brutils

--- grain_bill.py
+++ grain_bill.py
@@ -75,39 +75,57 @@ def hot_to_cold_water_volume(hot_water_volume_g):
 def cold_to_hot_water_volume(cold_water_volume_g):
   return cold_water_volume_g / 0.96
 
-def calc_mash_and_sparge_water(fermentables, batch_size_g, mash_ratio_quarts_per_lb, trub_loss_g, mash_tun_false_bottom_volume_g, equipment_loss_g, evap_rate_gal_per_hour, boil_length_min):
+def calc_mash_and_sparge_water(fermentables, batch_size_g, mash_ratio_quarts_per_lb, trub_loss_g, mash_tun_false_bottom_volume_g, sparge_water_false_bottom_volume_g, equipment_loss_g, evap_rate_gal_per_hour, boil_length_min):
   total_grain_weight_lbs = 0
   for fermentable in fermentables:
     if not fermentable.is_extract:
       total_grain_weight_lbs += fermentable.lbs_in_recipe
 
-  grain_water_loss_g = total_grain_weight_lbs * 0.2
+  data = {}
+
+  total_water_g = batch_size_g
+  data['starting_water'] = total_water_g
+
+  total_water_g += trub_loss_g
+  data['final_boil_volume'] = total_water_g
+
+
   evap_rate_gal_per_min = evap_rate_gal_per_hour/60
   evap_loss_g = evap_rate_gal_per_min*boil_length_min
-  total_hot_water_lost = grain_water_loss_g + trub_loss_g + equipment_loss_g + evap_loss_g
+  total_water_g += evap_loss_g
+  data['after_evaporation'] = total_water_g
 
-  total_water_needed = batch_size_g + hot_to_cold_water_volume(total_hot_water_lost)
+  total_water_g += equipment_loss_g
+  data['after_equipment_loss'] = total_water_g
+
+
+  grain_water_loss_g = total_grain_weight_lbs * 0.2
+  total_water_g += grain_water_loss_g
+  data['after_grain_loss'] = total_water_g
+
+  # account for shrinkage
+  total_water_g = cold_to_hot_water_volume(total_water_g)
+  data['with_shrinkage'] = total_water_g
 
   mash_water_cold_gal = hot_to_cold_water_volume(mash_tun_false_bottom_volume_g) + hot_to_cold_water_volume(total_grain_weight_lbs * mash_ratio_quarts_per_lb)/4
-  sparge_water_g = total_water_needed - mash_water_cold_gal
+  sparge_water_g = sparge_water_false_bottom_volume_g + total_water_g - mash_water_cold_gal
 
-  return {
-    'mash': mash_water_cold_gal,
-    'sparge': sparge_water_g,
-    'lost': hot_to_cold_water_volume(total_hot_water_lost),
-    'total': total_water_needed
-  }
-
-
+  data['mash'] = mash_water_cold_gal
+  data['sparge'] = sparge_water_g
+  data['total'] = total_water_g
+  return data
 
 
 def main():
+  # fermentables = [
+  #   Fermentable("Wheat", 38, 0.2, 8),
+  #   Fermentable("Honey", 33, 0.2, 0, True),
+  #   Fermentable("Two-row", 36, 0.6, 2)
+  # ]
   fermentables = [
-    Fermentable("Wheat", 38, 0.2, 8),
-    Fermentable("Honey", 33, 0.2, 0, True),
-    Fermentable("Two-row", 36, 0.6, 2)
+    Fermentable("Wheat", 38, 1, 8),
   ]
-  get_fermentables_amount(44, 5.5, fermentables, 0.68)
+  get_fermentables_amount(39.95, 5.5, fermentables, 0.68)
 
   print("Fermentables: ")
   for f in fermentables:
@@ -115,10 +133,10 @@ def main():
   srm = get_estimated_srm(5.5, fermentables)
   print("Estimated SRM: {0} ({1})".format(srm, get_srm_descriptor(srm)))
 
-  water_details = calc_mash_and_sparge_water(fermentables, 5, 1.33, 0.25, 1, 0.5, 0.5, 60)
+  water_details = calc_mash_and_sparge_water(fermentables, 5, 1.33, 0.5, 0, 0, 1, 0.275, 90)
   print('Mash Water: {0} gallons'.format(water_details['mash']))
   print('Sparge Water: {0} gallons'.format(water_details['sparge']))
-  print('Total water lost: {0} gallons'.format(water_details['lost']))
+  # print('Total water lost: {0} gallons'.format(water_details['lost']))
   print('Total water: {0} gallons'.format(water_details['total']))
 
 main()
